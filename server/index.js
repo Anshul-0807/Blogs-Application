fe919d531fb047df920b8018ecfc6b7fb198496d
Anshul patle <patleanshul920@gmail.com>
2023-01-26 19:16:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from 'dotenv';
 import Connection from "./database/db.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use('/', Router)
 
 const PORT = 8000;
 
