ca081c59b590106d0633c97e7ee0979e6a3ca199
Anshul patle <patleanshul920@gmail.com>
2023-01-26 18:20:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import dotenv from 'dotenv';
+import Connection from "./database/db.js";
+
+dotenv.config();
 
 const app = express();
 
 const PORT = 8000;
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+const USERNAME = process.env.DB_USERNAME;
+
+const PASSWORD = process.env.DB_PASSWORD;
+
+Connection( USERNAME, PASSWORD);
