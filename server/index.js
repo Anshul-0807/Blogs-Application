9255922db6d494df42865aab2b77dd8c09af8888
Anshul patle <patleanshul920@gmail.com>
2023-01-27 22:12:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,10 +11,11 @@ app.use('/', Router)
 
 const PORT = 8000;
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
 
 const USERNAME = process.env.DB_USERNAME;
 
 const PASSWORD = process.env.DB_PASSWORD;
 
 Connection( USERNAME, PASSWORD);
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
