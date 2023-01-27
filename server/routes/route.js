36f7f1e52775a3dfafdbf983c31fd8676917eeb0
Anshul patle <patleanshul920@gmail.com>
2023-01-27 22:12:40 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signupUser } from '../controller/user-controller';
+import { signupUser } from '../controller/user-controller.js';
 
 const router = express.Router();
 
