6045175bc95af0e01d4adf689d56a60d356e0e04
Anshul patle <patleanshul920@gmail.com>
2023-01-28 19:06:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ import express from "express";
 import dotenv from 'dotenv';
 import Connection from "./database/db.js";
 import Router from "./routes/route.js";
+import cors from 'cors';
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', Router)
 
 const PORT = 8000;
