e90b7a0903daeb5328908121074d4e274cbc5ff0
Anshul patle <patleanshul920@gmail.com>
2023-01-28 08:59:22 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,8 +1,33 @@
 import axios from 'axios';
+import { response } from 'express';
 
 const API_URL = 'http://localhost:8000'
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
-    
+    timeout: 10000,
+    headers: {
+        "content-type": 'application/json'
+    }
 })
+
+axiosInstance.interceptors.request.use(
+    function(config){
+        return config;
+    },
+    function(error){
+        return Promise.reject(error);
+    }
+)
+
+axiosInstance.interceptors.response.use(
+    function(response){
+        // stop global loader here
+        return processResponse(response);
+    },
+    function(error){
+        // stop global loader here
+        return Promise.reject(processError(response));
+
+    }
+)
