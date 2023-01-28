474a8db8e867e7a50e95e8d31caf36dbf04bbad9
Anshul patle <patleanshul920@gmail.com>
2023-01-28 19:26:53 +0530

    Update user-controller.js

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,8 +1,11 @@
 import User from "../model/user.js";
+import bcrypt from 'bcrypt';
 
 export const signupUser = async (request, response) => {
    try {
-    const user = request.body;
+      // const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(request.body.password, 10);
+    const user = {username: request.body.username, name: request.body.name, password: hashedPassword};
     
     const newUser = new User(user);
 
