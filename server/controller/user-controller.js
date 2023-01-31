cbbd1fb2be399a9838d29fb0b505557294b455bf
Anshul patle <patleanshul920@gmail.com>
2023-02-01 00:53:22 +0530

    Update user-controller.js

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,6 +1,11 @@
-import User from "../model/user.js";
+
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import Token from "../model/token.js";
+import User from "../model/user.js";
+
+dotenv.config();
 
 export const signupUser = async (request, response) => {
    try {
@@ -19,22 +24,27 @@ export const signupUser = async (request, response) => {
    }
 }
 
-export const loginUser = async(request, response) => {
-   let user = await User.findOne({username: request.body.username});
-   if(!user){
-      return response.status(400).json({ msg: "Username does not match"})
+export const loginUser = async (request, response) => {
+   let user = await User.findOne({ username: request.body.username });
+   if (!user) {
+       return response.status(400).json({ msg: 'Username does not match' });
    }
+
    try {
-     let match = await bcrypt.compare(request.body.parser, user.password);
-     if(match){
-          const accessToken = jwt.sign(user.ToJSON(), )
+   let match = await bcrypt.compare(request.body.password, user.password);
+   if (match) {  
+      const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
+      const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+          
+          const newToken = new Token({ token: refreshToken });
+            await newToken.save();
 
-          const refreshToken = 
+          return response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken, name: user.name, username: user.username})
 
-     } else{
-      return  response.status(400).json({msg: 'password does not match'});
-     }
-   } catch (error) {
-      
-   }
+         } else {
+            response.status(400).json({ msg: 'Password does not match' })
+        }
+    } catch (error) {
+        response.status(500).json({ msg: 'error while login the user' })
+    }
 }
