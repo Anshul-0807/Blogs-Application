69c1ec90e290f3d4fa6c67fb2b9d311ba642f07a
Anshul patle <patleanshul920@gmail.com>
2023-02-01 00:52:57 +0530

    Create DataProvider.jsx

---
+++ client/src/context/DataProvider.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react"
+
+
+export const DataContext = createContext(null); 
+
+
+const DataProvider = ({children}) => {
+
+    const [account, setAccount] = useState({username:'', name: ''} )
+    
+  return(
+    <DataContext.Provider value={{
+    account,
+    setAccount
+    }}>
+    {children}
+    </DataContext.Provider>
+  )
+}
+
+export default DataProvider;
