10e7a05c72263fba2d78f06f9d7d168ac1aa21ba
Anshul patle <patleanshul920@gmail.com>
2023-02-01 00:53:32 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -4,6 +4,6 @@ import { signupUser, loginUser } from '../controller/user-controller.js';
 const router = express.Router();
 
 router.post('/signup', signupUser );
-router.post('/login', loginUser)
+router.post('/login', loginUser);
 
 export default router;
