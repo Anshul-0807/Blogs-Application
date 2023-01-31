cf6da0c2dea4a2b806ce009f1071bc449fab2f83
Anshul patle <patleanshul920@gmail.com>
2023-02-01 00:52:37 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/account/Login';
+import DataProvider from './context/DataProvider';
 
 
 function App() {
   return (
    <div style={{ marginTop: 64 }} >
+   <DataProvider>
    <Login/>
+   </DataProvider>
    </div>
   );
 }
