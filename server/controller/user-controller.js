1dd74c06965f6d8472683e0f189965df752834a0
Anshul patle <patleanshul920@gmail.com>
2023-02-01 18:00:43 +0530

    Update user-controller.js

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -25,23 +25,23 @@ export const signupUser = async (request, response) => {
 }
 
 export const loginUser = async (request, response) => {
-   let user = await User.findOne({ username: request.body.username });
-   if (!user) {
-       return response.status(400).json({ msg: 'Username does not match' });
-   }
+    let user = await User.findOne({ username: request.body.username });
+    if (!user) {
+        return response.status(400).json({ msg: 'Username does not match' });
+    }
 
-   try {
-   let match = await bcrypt.compare(request.body.password, user.password);
-   if (match) {  
-      const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
-      const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
-          
-          const newToken = new Token({ token: refreshToken });
+    try {
+        let match = await bcrypt.compare(request.body.password, user.password);
+        if (match) {
+            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
+            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+            
+            const newToken = new Token({ token: refreshToken });
             await newToken.save();
-
-          return response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken, name: user.name, username: user.username})
-
-         } else {
+        
+            response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken,name: user.name, username: user.username });
+        
+        } else {
             response.status(400).json({ msg: 'Password does not match' })
         }
     } catch (error) {
