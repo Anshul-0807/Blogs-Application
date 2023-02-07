46702cd83ca1576c90795dd5117b91606bb5d807
Anshul patle <patleanshul920@gmail.com>
2023-02-07 17:43:55 +0530

    Create CreatePost.js

---
+++ client/src/components/create/CreatePost.js
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+import React from 'react';
+
+const CreatePost = () => {
+  return (
+    <div>hello from CreatePost</div>
+  )
+}
+
+export default CreatePost;
