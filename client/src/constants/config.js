b77a4c47997db984874540d97ffc4afd150ae698
Anshul patle <patleanshul920@gmail.com>
2023-02-09 00:26:01 +0530

    Update config.js

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -28,5 +28,6 @@ export const API_NOTOFICATION_MESSAGE = {
 // NEED SERVICE CALL: { url: '/' , method: 'POST, GET, PUT, DELETE' params: true/false, query: true/false}
 export const SERVICE_URLS = {
     userSignup: { url: '/signup', method: 'POST'},
-    userLogin: { url: '/login', method: 'POST' }
+    userLogin: { url: '/login', method: 'POST' },
+    uploadFile: { url: '/file/upload', method: 'POST'}
 }
