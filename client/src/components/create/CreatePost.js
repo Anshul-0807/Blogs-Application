bbedc855e5087c70c040db433b18b8c3375f4f28
Anshul patle <patleanshul920@gmail.com>
2023-02-08 18:46:25 +0530

    Update CreatePost.js

--- client/src/components/create/CreatePost.js
+++ client/src/components/create/CreatePost.js
@@ -1,5 +1,6 @@
-import { Box, styled, FormControl } from '@mui/material';
+import { Box, styled, FormControl, InputBase, Button, TextareaAutosize } from '@mui/material';
 import { AddCircle as Add } from '@mui/icons-material';
+import { useState, useEffect } from 'react';
 
 const Container = styled(Box)`
 margin: 50px 100px
@@ -11,20 +12,83 @@ const Image = styled('img')({
   objectFit: 'cover'
 })
 
+const StyledFormControl = styled(FormControl)`
+margin-top: 10px;
+display: flex;
+flex-direction: row;
+`
+const InputTextFeild = styled(InputBase)`
+ flex: 1;
+ margin: 0 30px;
+ font-size: 25px;
+`;
+
+const Textarea = styled(TextareaAutosize)`
+width: 100%;
+margin-top: 50px;
+font-size: 18px;
+border: none;
+ &:focus-visible{
+ outline: none;
+}
+`;
+
+const initialPost = {
+  title: '',
+  description: '',
+  picture: '',
+  username: '',
+  categories: '',
+  createdDate: new Date()
+
+}
+
 const CreatePost = () => {
 
   const url = 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
+
+  const [post, setPost] = useState(initialPost);
+  const [file, setFile] = useState('')
+
+  useEffect(() => {
+    const getImage = () => {
+      if(file){
+        const data= new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+      }
+    }
+  }, [])
+  
+
+  const handleChange = (e) => {
+   setPost({...post, [e.target.name]: e.target.value})
+  }
 
   return (
    <Container>
     <Image src={url} alt="banner"/>
 
-    <FormControl>
+    <StyledFormControl>
     <label htmlFor='fileInput'>
-      <Add/>
+      <Add fontSize='large' color='action'/>
     </label>
-      <input type="file" />
-    </FormControl>
+      <input
+       type="file" 
+        id='fileInput'
+        style={{display: 'none'}}
+        onChange={(e) => setFile(e.target.files[0]) }
+       />
+       <InputTextFeild placeholder='Title' onChange={(e)  => handleChange(e)} name="title" />
+       <Button variant='contained'>Publish</Button>
+    </StyledFormControl>
+
+   <Textarea 
+    minRows={5}
+    placeholder='Tell your story.......'
+    onChange={(e)  => handleChange(e)}
+    name="descreption"
+   />
    </Container>
   )
 }
