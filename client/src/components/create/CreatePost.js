7a8c7e806481a9a920590af5587915dc0ff6f8c6
Anshul patle <patleanshul920@gmail.com>
2023-02-08 13:58:45 +0530

    Update CreatePost.js

--- client/src/components/create/CreatePost.js
+++ client/src/components/create/CreatePost.js
@@ -1,10 +1,31 @@
-import { Box } from '@mui/material';
+import { Box, styled, FormControl } from '@mui/material';
+import { AddCircle as Add } from '@mui/icons-material';
 
-import React from 'react';
+const Container = styled(Box)`
+margin: 50px 100px
+`
+
+const Image = styled('img')({
+  width: '100%',
+  height: '50vh',
+  objectFit: 'cover'
+})
 
 const CreatePost = () => {
+
+  const url = 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
+
   return (
-    <div>hello from CreatePost</div>
+   <Container>
+    <Image src={url} alt="banner"/>
+
+    <FormControl>
+    <label htmlFor='fileInput'>
+      <Add/>
+    </label>
+      <input type="file" />
+    </FormControl>
+   </Container>
   )
 }
 
