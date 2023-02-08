c014b1c8ff9d7ba8771ca4bbb50368ac7ae04863
Anshul patle <patleanshul920@gmail.com>
2023-02-09 00:25:57 +0530

    Update Categories.jsx

--- client/src/components/home/Categories.jsx
+++ client/src/components/home/Categories.jsx
@@ -1,48 +1,64 @@
-import React from 'react'
-import { Button,Table, TableBody, TableCell, TableHead, TableRow , styled} from '@mui/material'
-import { categories } from '../../constants/data';
-import  { Link } from 'react-router-dom';
+import React from "react";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  styled,
+} from "@mui/material";
+import { categories } from "../../constants/data";
+import { Link, useSearchParams } from "react-router-dom";
 
 const StyledTable = styled(Table)`
   border: 1px solid rgba(224, 224, 224, 1);
 `;
 
 const StyledButton = styled(Button)`
- margin: 20px;
- width: 85%;
- background: #6495ED;
- color: #fff;
-`
+  margin: 20px;
+  width: 85%;
+  background: #6495ed;
+  color: #fff;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
 
 const Categories = () => {
+  const [SearchParams] = useSearchParams();
+  const category = SearchParams.get("category");
+
   return (
-   <>
-    <Link to='/create' style={{ textDecoration: 'none'}}>
-    <StyledButton variant='contained'>Create Blog</StyledButton>
-    </Link>
-    <StyledTable>
+    <>
+      <StyledLink
+        to={`/create?category=${category || ""}`}
+        style={{ textDecoration: "none" }}
+      >
+        <StyledButton variant="contained">Create Blog</StyledButton>
+      </StyledLink>
+      <StyledTable>
         <TableHead>
-            <TableRow>
-                <TableCell>
-                    All Categories
-                </TableCell>
-            </TableRow>
+          <TableRow>
+            <TableCell>
+              <StyledLink to="/">All Categories</StyledLink>
+            </TableCell>
+          </TableRow>
         </TableHead>
         <TableBody>
-        {
-            categories.map(category => (
-                <TableRow key={category.id}>
-                <TableCell>
-                    {category.type}
-                </TableCell>
+          {categories.map((category) => (
+            <TableRow key={category.id}>
+              <TableCell>
+                <StyledLink to={`/?category=${category.type}`}>{category.type}</StyledLink>
+              </TableCell>
             </TableRow>
-            ))
-        }
-           
+          ))}
         </TableBody>
-    </StyledTable>
-   </>
-  )
-}
+      </StyledTable>
+    </>
+  );
+};
 
 export default Categories;
