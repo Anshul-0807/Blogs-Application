77b1d12030a19b010f11b3ee73b27910dbe3fd81
Anshul patle <patleanshul920@gmail.com>
2023-02-10 15:18:41 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { signupUser, loginUser } from '../controller/user-controller.js';
-import { uploadImage } from '../controller/image-controller.js';
+import { uploadImage , getImage} from '../controller/image-controller.js';
 import upload from '../utils/upload.js'
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post('/signup', signupUser );
 router.post('/login', loginUser);
 
 router.post('/file/upload', upload.single('file'), uploadImage);
+router.get('/file/:filename', getImage);
 
 export default router;
