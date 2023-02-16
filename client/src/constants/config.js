048194e0ce0c8a057d452d61c36bf9ca29ca698f
Anshul patle <patleanshul920@gmail.com>
2023-02-16 08:11:05 +0530

    Update config.js

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,33 +1,35 @@
 // API_NOTIFICATION_MESSAGE
 
 export const API_NOTIFICATION_MESSAGES = {
-     loading: {
-        title: "loading...",
-        message: "Data is being loaded, please wait"
-     },
-     success: {
-        title: "success",
-        message: "Data successfully loaded"
-     },
-     responseFailure: {
-        title: "Error",
-        message: "An error occured while fetching response from the server. please try again "
-     },
-     request: {
-        title:  "Error",
-        message: "An error occured while parsing request data"
-     },
-     networkError: {
-        title: "Error",
-        message: "unable to connect with the server, please check network connectivity and try agian later.."
-     }
-}
+  loading: {
+    title: "loading...",
+    message: "Data is being loaded, please wait",
+  },
+  success: {
+    title: "success",
+    message: "Data successfully loaded",
+  },
+  responseFailure: {
+    title: "Error",
+    message:
+      "An error occured while fetching response from the server. please try again ",
+  },
+  request: {
+    title: "Error",
+    message: "An error occured while parsing request data",
+  },
+  networkError: {
+    title: "Error",
+    message:
+      "unable to connect with the server, please check network connectivity and try agian later..",
+  },
+};
 
 //  API service call
 // SAMPLE REQUEST
 // NEED SERVICE CALL: { url: '/' , method: 'POST, GET, PUT, DELETE' params: true/false, query: true/false}
 export const SERVICE_URLS = {
-    userSignup: { url: '/signup', method: 'POST'},
-    userLogin: { url: '/login', method: 'POST' },
-    uploadFile: { url: '/file/upload', method: 'POST' },
-}
+  userSignup: { url: "/signup", method: "POST" },
+  userLogin: { url: "/login", method: "POST" },
+  uploadFile: { url: "/file/upload", method: "POST" },
+};
