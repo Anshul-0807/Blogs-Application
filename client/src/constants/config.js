10bf67678477846af13094f5fc1f1ec8f7b77362
Anshul patle <patleanshul920@gmail.com>
2023-02-20 18:25:37 +0530

    Update config.js

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -32,4 +32,5 @@ export const SERVICE_URLS = {
   userSignup: { url: "/signup", method: "POST" },
   userLogin: { url: "/login", method: "POST" },
   uploadFile: { url: "/file/upload", method: "POST" },
+  createPost: {url: 'create', method: 'POST'},
 };
