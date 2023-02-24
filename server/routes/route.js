eb31a1db6ecb4b6d30812b1b718fa0db39dcfd9d
Anshul patle <patleanshul920@gmail.com>
2023-02-24 15:32:03 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { signupUser, loginUser } from '../controller/user-controller.js';
 import { uploadImage , getImage} from '../controller/image-controller.js';
 import { createPost } from '../controller/post-controller.js';
-
+import {authenticateToken} from '../controller/jwt-controller.js';
 
 import upload from '../utils/upload.js';
 
@@ -14,7 +14,7 @@ router.post('/login', loginUser);
 router.post('/file/upload', upload.single('file'), uploadImage);
 router.get('/file/:filename', getImage);
 
-router.post('/create', createPost);
+router.post('/create',authenticateToken, createPost);
 
 
 export default router;
