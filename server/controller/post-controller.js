82ec27102321ca45fe7a45c0e65b3806f64feb04
Anshul patle <patleanshul920@gmail.com>
2023-02-24 15:31:50 +0530

    Update post-controller.js

--- server/controller/post-controller.js
+++ server/controller/post-controller.js
@@ -1,8 +1,15 @@
 import { response } from "express";
-
+import Post from "../model/post.js";
 
 
 
 export const createPost = async (request, response) => {
-    
+   try{
+    const post =  await new Post(request.body);
+    post.save();
+
+    return response.status(200).json('post saved successfully');
+   } catch(error){
+    return response.status(500).json(error);
+    }
 }
