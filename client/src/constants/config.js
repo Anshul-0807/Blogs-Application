7a63d98cf6cce9dd1fbf98383a7c8b7061667bec
Anshul patle <patleanshul920@gmail.com>
2023-02-24 15:31:30 +0530

    Update config.js

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -33,4 +33,6 @@ export const SERVICE_URLS = {
   userLogin: { url: "/login", method: "POST" },
   uploadFile: { url: "/file/upload", method: "POST" },
   createPost: {url: 'create', method: 'POST'},
+  getAllPosts: { url: '/posts', method: 'GET'},
+
 };
