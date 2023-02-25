d737dde864f9ee1882e985efe54339eb6b57761a
Anshul patle <patleanshul920@gmail.com>
2023-02-25 15:31:49 +0530

    Update Posts.jsx

--- client/src/components/home/post/Posts.jsx
+++ client/src/components/home/post/Posts.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import { useState, useEffect} from 'react';
-import {API} from '../../../service/api';
+import React from "react";
+import { useState, useEffect } from "react";
+import { API } from "../../../service/api";
+import { Box, Grid } from "@mui/material";
 
+// component
+import Post from "./Post";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
 
-   const [posts , setPosts] = useState([]);
-
-    useEffect(() => {
-     const fetchData = async () => {
-      let response =  await API.getAllPosts();
-       if(response.isSuccess){
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await API.getAllPosts();
+      if (response.isSuccess) {
         setPosts(response.data);
-       }
-     }
-     fetchData();
-    }, [])
-    
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-   <>
-    {
-        posts && posts.length > 0 ? posts.map(post => (
-            <div>hello</div>
-        )) : <div>No data available in display</div>
-    }
-   </>
-  )
-}
+    <>
+      {
+        posts && posts.length > 0 ? posts.map( post => ( 
+          <Grid item lg = {3} sm={4} xs={12}>
+             <Post post={post} />
+          </Grid>
+      )) : 
+        <Box style={{ color: "#878787", margin: "30px 80px", fontsize: 18 }}>
+          No data available in display
+        </Box>
+      }
+    </>
+  );
+};
 
 export default Posts;
