b3568e6b6ecdab740a35aabca1ee95e4123f009b
Anshul patle <patleanshul920@gmail.com>
2023-03-03 18:40:43 +0530

    Update Post.jsx

--- client/src/components/home/post/Post.jsx
+++ client/src/components/home/post/Post.jsx
@@ -3,10 +3,19 @@ import { Box, Typography, styled} from '@mui/material';
 
 const Container = styled(Box)`
 border: 1px solid #d3cede;
+border-radius: 10px;
+margin: 10px;
+height: 350px;
+ & > p {
+    padding: 0 5px 5px 5px;
+}
 `;
 
 const Image = styled('img')`
- width: '100%'
+ width: '100%',
+ borderRadius: '10px 10px 0 0 ',
+ objectFit: 'cover',
+ height: 150,
 `;
 
 const Post = ({post}) => {
