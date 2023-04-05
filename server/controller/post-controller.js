0806d9b5bb7379520f100f2de544e218b352f37d
Anshul patle <patleanshul920@gmail.com>
2023-04-06 00:12:48 +0530

    Update post-controller.js

--- server/controller/post-controller.js
+++ server/controller/post-controller.js
@@ -1,25 +1,23 @@
 import { response } from "express";
 import Post from "../model/post.js";
 
-
-
 export const createPost = async (request, response) => {
-   try{
-    const post =  await new Post(request.body);
+  try {
+    const post = await new Post(request.body);
     post.save();
 
-    return response.status(200).json('post saved successfully');
-   } catch(error){
+    return response.status(200).json("post saved successfully");
+  } catch (error) {
     return response.status(500).json(error);
-    }
-}
+  }
+};
 
 export const getAllPosts = async (request, response) => {
-    try{
-        let posts = await Post.find({});
+  try {
+    let posts = await Post.find({});
 
-        return response.status(200).json(posts);
-    } catch( error ) {
-     return response.status(500).json({msg: error.message});
-    }
-}
+    return response.status(200).json(posts);
+  } catch (error) {
+    return response.status(500).json({ msg: error.message });
+  }
+};
