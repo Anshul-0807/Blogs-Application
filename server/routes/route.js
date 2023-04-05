95b1cea22b4bb50e0724d7911ec0dfcc332e3ce9
Anshul patle <patleanshul920@gmail.com>
2023-04-06 00:12:51 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import { signupUser, loginUser } from '../controller/user-controller.js';
-import { uploadImage , getImage} from '../controller/image-controller.js';
-import { createPost, getAllPosts } from '../controller/post-controller.js';
-import {authenticateToken} from '../controller/jwt-controller.js';
+import express from "express";
+import { signupUser, loginUser } from "../controller/user-controller.js";
+import { uploadImage, getImage } from "../controller/image-controller.js";
+import { createPost, getAllPosts } from "../controller/post-controller.js";
+import { authenticateToken } from "../controller/jwt-controller.js";
 
-import upload from '../utils/upload.js';
+import upload from "../utils/upload.js";
 
 const router = express.Router();
 
-router.post('/signup', signupUser );
-router.post('/login', loginUser);
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
 
-router.post('/file/upload', upload.single('file'), uploadImage);
-router.get('/file/:filename', getImage);
+router.post("/file/upload", upload.single("file"), uploadImage);
+router.get("/file/:filename", getImage);
 
-router.post('/create', authenticateToken, createPost);
-router.get('/posts', authenticateToken, getAllPosts);
+router.post("/create", authenticateToken, createPost);
+router.get("/posts", authenticateToken, getAllPosts);
 
 export default router;
