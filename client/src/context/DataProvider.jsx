4ed19a3ab22ef906601b146fd2eb9ffc1bfc5362
Anshul patle <patleanshul920@gmail.com>
2023-05-14 20:31:20 +0530

    Update DataProvider.jsx

--- client/src/context/DataProvider.jsx
+++ client/src/context/DataProvider.jsx
@@ -1,21 +1,20 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
 
+export const DataContext = createContext(null);
 
-export const DataContext = createContext(null); 
+const DataProvider = ({ children }) => {
+  const [account, setAccount] = useState({ username: "", name: "" });
 
-
-const DataProvider = ({children}) => {
-
-    const [account, setAccount] = useState({username:'', name: ''} )
-    
-  return(
-    <DataContext.Provider value={{
-    account,
-    setAccount
-    }}>
-    {children}
+  return (
+    <DataContext.Provider
+      value={{
+        account,
+        setAccount,
+      }}
+    >
+      {children}
     </DataContext.Provider>
-  )
-}
+  );
+};
 
 export default DataProvider;
