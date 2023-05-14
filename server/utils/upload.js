8b3ef49070d0e7b961b431229562c872ca27f51f
Anshul patle <patleanshul920@gmail.com>
2023-05-14 20:31:34 +0530

    Update upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,9 +1,6 @@
-import multer from 'multer';
-import { GridFsStorage } from 'multer-gridfs-storage';
-import dotenv from 'dotenv';
-
-
-
+import multer from "multer";
+import { GridFsStorage } from "multer-gridfs-storage";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -11,20 +8,19 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 const storage = new GridFsStorage({
-   url :  `mongodb://${username}:${password}@ac-dccrdey-shard-00-00.fjilivc.mongodb.net:27017,ac-dccrdey-shard-00-01.fjilivc.mongodb.net:27017,ac-dccrdey-shard-00-02.fjilivc.mongodb.net:27017/?ssl=true&replicaSet=atlas-zsvfbg-shard-0&authSource=admin&retryWrites=true&w=majority`,
-   options: { useNewUrlParser: true },
-   file: (request, file) => {
-   const match = ["image/png", "image/jpg"];
+  url: `mongodb://${username}:${password}@ac-dccrdey-shard-00-00.fjilivc.mongodb.net:27017,ac-dccrdey-shard-00-01.fjilivc.mongodb.net:27017,ac-dccrdey-shard-00-02.fjilivc.mongodb.net:27017/?ssl=true&replicaSet=atlas-zsvfbg-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  options: { useNewUrlParser: true },
+  file: (request, file) => {
+    const match = ["image/png", "image/jpg"];
 
-   if(match.indexOf(file.memeType) === -1) 
-            return`${Date.now()}-blog-${file.originalname}`;
+    if (match.indexOf(file.memeType) === -1)
+      return `${Date.now()}-blog-${file.originalname}`;
 
-        return {
-            bucketName: "photos",
-            filename: `${Date.now()}-blog-${file.originalname}`
-        }
-    }
-
+    return {
+      bucketName: "photos",
+      filename: `${Date.now()}-blog-${file.originalname}`,
+    };
+  },
 });
 
-export default multer({storage}); 
+export default multer({ storage });
