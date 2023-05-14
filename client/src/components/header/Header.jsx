447bcf6891baedf396a0f254d3b391db95270d1d
Anshul patle <patleanshul920@gmail.com>
2023-05-14 20:30:55 +0530

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography, styled } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Component = styled(AppBar)`
   background: #ffffff;
   color: #000;
 `;
 
- const Container = styled(Toolbar)`
- justify-content: center;
- & > a {
+const Container = styled(Toolbar)`
+  justify-content: center;
+  & > a {
     padding: 20px;
     color: #000;
     text-decoration: none;
- }
- `;
+  }
+`;
 
 const Header = () => {
   return (
-   <Component>
-    <Container>
-        <Link to='/'>HOME</Link>
-        <Link to='/about'>ABOUT</Link>
-        <Link to='/contact'>CONTACT</Link>
-        <Link to='/login'>LOGOUT</Link>
-    </Container>
-   </Component>
-  )
-}
+    <Component>
+      <Container>
+        <Link to="/">HOME</Link>
+        <Link to="/about">ABOUT</Link>
+        <Link to="/contact">CONTACT</Link>
+        <Link to="/login">LOGOUT</Link>
+      </Container>
+    </Component>
+  );
+};
 
-export default Header
+export default Header;
