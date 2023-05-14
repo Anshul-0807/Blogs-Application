92f049487a3e842da15a62af4b0b15b55cbb6631
Anshul patle <patleanshul920@gmail.com>
2023-05-14 20:31:30 +0530

    Update post.js

--- server/model/post.js
+++ server/model/post.js
@@ -1,32 +1,32 @@
 import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    descreption: {
-        type: String,
-        required: true
-    },
-    picture: {
-        type: String,
-        required: true,
-    },
-    username: {
-        type: String,
-        required: true,
-    },
-    categories: {
-        type: String,
-        required: true,
-    },
-    createdDate: {
-        type: Date
-    }
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  descreption: {
+    type: String,
+    required: true,
+  },
+  picture: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  categories: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+  },
 });
 
-const post = mongoose.model('post', postSchema);
+const post = mongoose.model("post", postSchema);
 
 export default post;
